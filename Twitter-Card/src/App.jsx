299839d7befaae659ae4397cb176f70e4dbff9ce
Kevin Feliz Henriquez <kfh.tech.programmer@gmail.com>
2023-06-13 21:25:53 -1100

    Adding the project with first functions and styles.

---
+++ Twitter-Card/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+export function App (){
+    return (
+        <article>
+            <header>
+                <img src="https://unavatar.io/github/programmerKFH" alt="el avatar de programmerKFH" />
+                <div>
+                    <strong>Kevin Feliz Henriquez</strong>
+                    <span>@programmerKFH</span>
+                </div>            
+            </header>
+
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+    )
+}
