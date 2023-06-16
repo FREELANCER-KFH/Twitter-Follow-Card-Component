a1bb656be384f7e1c9f4784dea3521d3f57c2e27
Kevin Feliz Henriquez <kfh.tech.programmer@gmail.com>
2023-06-15 18:52:33 -1100

    Modifying the styles of the Twitter-Card component, the App container and the document's body.

--- Twitter-Card/src/App.jsx
+++ Twitter-Card/src/App.jsx
@@ -4,11 +4,13 @@ import { TwitterFollowCard } from './TwitterFollowCard'
 export function App (){
     return (
         <>
-            <TwitterFollowCard userName="programmerKFH" name="Kevin Feliz Henriquez"/>
-            <TwitterFollowCard userName="Benignomnez" name="Benigno Martinez"/>
-            <TwitterFollowCard userName="pancakecodes" name="Sabrina Hernandez"/>
-            <TwitterFollowCard userName="codebyeli" name="Hector Encarnacion"/>
-            <TwitterFollowCard userName="teresa08" name="Maria Teresa Soriano"/>
+            <div className='container'>
+                <TwitterFollowCard userName="programmerKFH" name="Kevin Feliz Henriquez"/>
+                <TwitterFollowCard userName="Benignomnez" name="Benigno Martinez"/>
+                <TwitterFollowCard userName="pancakecodes" name="Sabrina Hernandez"/>
+                <TwitterFollowCard userName="codebyeli" name="Hector Encarnacion"/>
+                <TwitterFollowCard userName="teresa08" name="Maria Teresa Soriano"/>
+            </div>
         </>
     )
 }
