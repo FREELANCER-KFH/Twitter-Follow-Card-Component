dc19d876f6f719eb6a25d3a05784dc87a678913c
Kevin Feliz Henriquez <kfh.tech.programmer@gmail.com>
2023-06-18 00:03:16 -1100

    Making the commited files in this commit more legibles.

--- Twitter-Card/src/App.jsx
+++ Twitter-Card/src/App.jsx
@@ -2,14 +2,25 @@ import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App (){
+
     return (
         <>
             <div className='container'>
-                <TwitterFollowCard userName="programmerKFH" name="Kevin Feliz Henriquez"/>
-                <TwitterFollowCard userName="Benignomnez" name="Benigno Martinez"/>
-                <TwitterFollowCard userName="pancakecodes" name="Sabrina Hernandez"/>
-                <TwitterFollowCard userName="codebyeli" name="Hector Encarnacion"/>
-                <TwitterFollowCard userName="teresa08" name="Maria Teresa Soriano"/>
+                <TwitterFollowCard 
+                    isFollowing userName="programmerKFH" name="Kevin Feliz Henriquez"
+                />
+                <TwitterFollowCard 
+                    isFollowing userName="Benignomnez" name="Benigno Martinez"
+                />
+                <TwitterFollowCard 
+                    isFollowing userName="pancakecodes" name="Sabrina Hernandez"
+                />
+                <TwitterFollowCard 
+                    isFollowing userName="codebyeli" name="Hector Encarnacion"
+                />
+                <TwitterFollowCard 
+                    isFollowing userName="teresa08" name="Maria Teresa Soriano"
+                />
             </div>
         </>
     )
--- Twitter-Card/src/TwitterFollowCard.jsx
+++ Twitter-Card/src/TwitterFollowCard.jsx
@@ -1,16 +1,23 @@
 export function TwitterFollowCard({userName, name, isFollowing}){
+    const text = isFollowing 
+        ? 'Siguiendo' 
+        : 'Seguir'
+    const buttonClassName = isFollowing 
+        ? 'tw-followCard-button is-following' 
+        : 'tw-followCard-button'
+
     return (
-        <article>
-            <header>
-                <img src = {`https://unavatar.io/github/${userName}`} alt="el avatar de usuario" />
-                <div>
+        <article className="tw-followCard">
+            <header className="tw-followCard-header">
+                <img className="tw-followCard-avatar" src = {`https://unavatar.io/github/${userName}`} alt="el avatar de usuario" />
+                <div className="tw-followCard-info">
                     <strong>{name}</strong>
-                    <span>@{userName}</span>
+                    <span className="tw-followCard-infoUserName">@{userName}</span>
                 </div>            
             </header>
 
             <aside>
-                <button>Seguir</button>
+                <button className={buttonClassName}>Seguir</button>
             </aside>
         </article>
     )
